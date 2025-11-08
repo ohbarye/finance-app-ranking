25caf154cfb9f0f3c978a1d5ff85edd014fc6783
ohbarye <over.rye@gmail.com>
2025-11-08 12:07:32 +0900

    Follow up the latest site structure

--- scrape.rb
+++ scrape.rb
@@ -7,16 +7,16 @@ def scrape_ios_ranking
   url = "https://apps.apple.com/jp/charts/iphone/%E3%83%95%E3%82%A1%E3%82%A4%E3%83%8A%E3%83%B3%E3%82%B9-apps/6015?chart=top-free"
   html = URI.open(url).read
   doc = Nokogiri::HTML(html)
-  items = doc.css('.chart li').map do |item|
-    rank = Integer(item.css('.we-lockup__rank').first.text)
-    title = item.css('.we-lockup__title').text.strip
-    developer = item.css('.we-lockup__subtitle').text.strip
+  items = doc.css('.shelf .grid li').map do |item|
+    rank = Integer(item.css('.ordinal').first.text)
+    title = item.css('h3').text.strip
+    # Developer is not available in the current HTML structure
+    # developer = item.css('.we-lockup__subtitle').text.strip
     url = item.css('a').attr('href').value
 
     {
       rank:,
       title:,
-      developer:,
       url:,
     }
   end.sort_by { |item| item[:rank] }
